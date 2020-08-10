713cf2753bcd7b22db4a3543731f179584b2ac02
Szabo Vilmos <vili8899s@gmail.com>
2020-08-10 19:32:03 +0200

    finished

--- src/components/People.js
+++ src/components/People.js
@@ -2,13 +2,17 @@ import React from 'react';
 import { useQuery } from 'react-query';
 import Person from './Person';
 
-const fetchPeople = async () => {
-  const res = await fetch('https://swapi.dev/api/people/');
+const fetchPeople = async (key, page) => {
+  const res = await fetch(`https://swapi.dev/api/people/?page=${page}`);
   return res.json();
 };
 
 const People = () => {
-  const { data, status } = useQuery('people', fetchPeople);
+  const [page, setPage] = useState(1);
+  const { resolvedData, latestData, status } = usePaginatedQuery(
+    ['people', page],
+    fetchPeople
+  );
 
   return (
     <div>
@@ -19,11 +23,30 @@ const People = () => {
       {status === 'error' && <div>Error fetching data</div>}
 
       {status === 'success' && (
-        <div>
-          {data.results.map((person) => (
-            <Person key={person.name} person={person} />
-          ))}
-        </div>
+        <>
+          <button
+            onClick={() => setPage((old) => Math.max(old - 1, 1))}
+            disabled={page === 1}
+          >
+            Previous Page
+          </button>
+          <span>{page}</span>
+          <button
+            onClick={() =>
+              setPage((old) =>
+                !latestData || !latestData.next ? old : old + 1
+              )
+            }
+            disabled={!latestData || !latestData.next}
+          >
+            Next page
+          </button>
+          <div>
+            {resolvedData.results.map((person) => (
+              <Planet key={person.name} person={person} />
+            ))}
+          </div>
+        </>
       )}
     </div>
   );
