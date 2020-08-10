78b2bdf144d2aa8a901e69a5fdad247241ebbc68
Szabo Vilmos <vili8899s@gmail.com>
2020-08-10 19:25:05 +0200

    finished

--- src/components/People.js
+++ src/components/People.js
@@ -3,9 +3,9 @@ import { useQuery } from 'react-query';
 import Person from './Person';
 
 const fetchPeople = async () => {
-  const res = await fetch('http://swapi.dev/api/people/');
+  const res = await fetch('https://swapi.dev/api/people/');
   return res.json();
-}
+};
 
 const People = () => {
   const { data, status } = useQuery('people', fetchPeople);
@@ -14,21 +14,19 @@ const People = () => {
     <div>
       <h2>People</h2>
 
-      {status === 'loading' && (
-        <div>Loading data</div>
-      )}
+      {status === 'loading' && <div>Loading data</div>}
 
-      {status === 'error' && (
-        <div>Error fetching data</div>
-      )}
+      {status === 'error' && <div>Error fetching data</div>}
 
       {status === 'success' && (
         <div>
-          { data.results.map(person => <Person key={person.name} person={person} /> ) }
+          {data.results.map((person) => (
+            <Person key={person.name} person={person} />
+          ))}
         </div>
-      )} 
+      )}
     </div>
   );
-}
- 
+};
+
 export default People;
